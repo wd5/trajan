483bf318f527ab6b30753968a3f8f319db04512d
Derek Stegelman <dstegelman@gmail.com>
2012-02-11 10:04:57 -0600

    org

--- core/admin.py
+++
@@ -1,5 +0,0 @@
-from core.models import *
-from django.contrib import admin
-
-admin.site.register(Category)
-admin.site.register(Page)
--- core/context_processor.py
+++
@@ -1,7 +0,0 @@
-from core.models import Page
-def pages(request):
-    context = {}
-    pages = Page.objects.all()
-    context['pages'] = pages
-    return context
-    
